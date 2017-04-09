b26aeda3ed70bdf4b7f093db88269fd5e1d936ce
AustinLangton <Austin.Langton@yahoo.com>
2017-04-09 18:13:17 -0400

    Update PipelinedSik.v

--- PipelinedSik.v
+++ PipelinedSik.v
@@ -343,7 +343,6 @@ always @(posedge clk)
 begin
 	//register values are handled
     	case (s[2])
-		$display("i: %d", i);
     	`Start: begin 
 		//this shouldn't happen, but if it does it's a nop
 		pcwriteflag = 0;
